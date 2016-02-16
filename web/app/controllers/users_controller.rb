1ea32baf388281b9500f44959bc1adf1657d4fb1
mohhsen7 <mohhsen@ymail.com>
2016-02-16 22:53:24 +0200

    signin

---
+++ web/app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+layout 'admin'
+before_action :confirmauthorized, :except => [:login, :attempt_login, :logout]
+  def index
+  end
+
+  def login
+
+  end
+
+  def logout
+	flash[:notice]="Logged out"
+	redirect_to(:action=>'login')
+	session[:user_id] = nil
+	session[:username] = nil
+  end
+
+  def attempt_login
+	if params[:username].present?&&params[:pasword].present?
+	found_user=User.where(:username=>params[:username]).first
+	if found_user
+		authorized_user=found_user.authenticate(params[:password])
+	end
+	end
+	if authorized_user
+	session[:user_id] = authorized_user.id
+	session[:username] = authorized_user.username
+	
+	redirect_to(:action=>'index')
+	else
+	flash[:notice]="Incorrect Username Or Password"
+	redirect_to(:action=>'login')
+	end
+  end
+
+end
---
+++ web/db/migrate/20160213024435_add_password_digest.rb
@@ -0,0 +1,8 @@
+class AddPasswordDigest < ActiveRecord::Migration
+  def up
+	add_column "users","password_digest", :string
+  end
+  def down
+	remove_column "users","password_digest"
+  end
+end
