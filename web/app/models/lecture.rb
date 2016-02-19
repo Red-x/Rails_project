d29a4056d9815bbed9dcc8895393625fd2b3f944
blacktiger_f18@hotmail.com <blacktiger_f18@hotmail.com>
2016-02-19 06:15:16 -0800

    Revert "full dataase"
    This reverts commit 0bda2cb12edeef3abc0ef746157e6d825fe0cc1f.
    
    Conflicts:
    	web/app/models/lecture.rb

--- web/app/models/lecture.rb
+++ web/app/models/lecture.rb
@@ -1,7 +1,12 @@
 class Lecture < ActiveRecord::Base
+<<<<<<< HEAD
 	 has_many :comments
 	 has_many :likes
 	 belongs_to :user
      has_many :slides
 
+=======
+	User has_many :commets
+	User has_many :likes
+>>>>>>> parent of 0bda2cb... full dataase
 end
